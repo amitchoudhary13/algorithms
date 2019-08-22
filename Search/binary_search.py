1972d9f42fa787aa9c34f100b0e579a783ccb312
amitchoudhary13 <cool.rock991@gmail.com>
2019-08-22 13:29:19 +0530

    Update binary_search.py

--- Search/binary_search.py
+++ Search/binary_search.py
@@ -3,15 +3,15 @@
 Created on Tue Aug 20 16:04:28 2019
 @author: Amit
 """
-def binarySearch(arr, l, r, x):
+def binary_search(arr, l, r, x):
     while r >= l:
         mid = int(l + (r - l)/2)
         if arr[mid] == x :
             return mid;
         elif arr[mid] > x:
-            return binarySearch(arr,l,mid-1,x)
+            return binary_search(arr,l,mid-1,x)
         elif arr[mid] < x:
-            return binarySearch(arr,mid+1,r,x)
+            return binary_search(arr,mid+1,r,x)
      else: 
         # Element is not present in the array 
         return -1
@@ -22,7 +22,7 @@ arr = [ 2, 3, 4, 10, 40 ]
 x = 10
   
 # Function call 
-result = binarySearch(arr, 0, len(arr)-1, x) 
+result = binary_search(arr, 0, len(arr)-1, x) 
   
 if result != -1: 
     print("Element is present at index %d" % result )
